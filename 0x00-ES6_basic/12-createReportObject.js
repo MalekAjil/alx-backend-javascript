dbb387176a548ebab6ae68722cc0aad99e4db202
MalekAjil <aboanasajil@gmail.com>
2024-12-27 07:09:39 -0800

    task12

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,9 +1,8 @@
-import createEmployeesObject from './11-createEmployeesObject.js';
 export default function createReportObject(employeesList) {
   return {
-	  allEmployees: { ...employeesList, },
-	  getNumberOfDepartments() {
-		  return Object.keys(this.allEmployees).length;
-	  },
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments() {
+      return Object.keys(this.allEmployees).length;
+    },
   };
 }
