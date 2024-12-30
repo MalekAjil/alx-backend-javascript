0d87ec79eab505ec9e563bd29477e9df672892e5
MalekAjil <aboanasajil@gmail.com>
2024-12-30 13:24:40 -0800

    task

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,3 +1,7 @@
 export default function createIteratorObject(report) {
-  
+  let allEmployees = [];
+  for (const department in report.allEmployees) {
+    allEmployees = allEmployees.concat(report.allEmployees[department]);
+  }
+  return allEmployees[Symbol.iterator]();
 }
