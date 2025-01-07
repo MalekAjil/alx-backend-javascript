402434181e60ed56fa3809b7ba6e7de1a3a4a13f
MalekAjil <aboanasajil@gmail.com>
2025-01-07 10:51:24 -0800

    task6

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,17 +1,16 @@
 export default class Building {
-  constructor(sqft) {
-    if (new.target === Building) {
-      throw new Error('Building is an abstract class and cannot be instantiated directly.');
+    constructor(sqft) {
+        if (new.target === Building) {
+            throw new Error("Building is an abstract class and cannot be instantiated directly.");
+        }
+        this._sqft = sqft;
     }
-    this._sqft = sqft;
-  }
 
-  get sqft() {
-    return this._sqft;
-  }
+    get sqft() {
+        return this._sqft;
+    }
 
-  evacuationWarningMessage() {
-    console.log(this);
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
+    evacuationWarningMessage() {
+        throw new Error("Class extending Building must override evacuationWarningMessage");
+    }
 }
