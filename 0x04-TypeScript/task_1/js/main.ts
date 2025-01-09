f2c7ec8ed10bc38ec570c437ea0cd6b4bcc9fbc1
MalekAjil <aboanasajil@gmail.com>
2025-01-09 13:28:14 -0800

    task1

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,26 @@
+export interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any; // This allows adding any attribute to the object
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
+
+// should print
+// Object
+// contract: false
+// firstName: "John"
+// fullTimeEmployee: false
+// lastName: "Doe"
+// location: "London"
